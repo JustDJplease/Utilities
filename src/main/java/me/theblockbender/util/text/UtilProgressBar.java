2e512a97beb69c70e2bc61b71305bf95fc269954
TheBlockBender <floris.jolink@gmail.com>
2018-06-29 21:34:57 +0200

    added progress bar utility

---
+++ src/main/java/me/theblockbender/util/text/UtilProgressBar.java
@@ -0,0 +1,33 @@
+package me.theblockbender.util.text;
+
+import org.bukkit.ChatColor;
+
+public class UtilProgressBar {
+    /**
+     * Get a progress bar
+     *
+     * @param current           how many points the user has
+     * @param max               the maximal number of <b>points</b> the user can get
+     * @param totalBars         the amount of <b>bars</b> these points should be divided over
+     * @param symbol            the symbol used to build the bars
+     * @param completedColor    the colour used to indicate bars that are completed
+     * @param notCompletedColor the colour used to indicate bars that are <b>not</b> completed
+     * @return progress bar string
+     */
+    public String getProgressBar(int current, int max, int totalBars, String symbol, String completedColor, String notCompletedColor) {
+        float percent = (float) current / max;
+        int progressBars = (int) ((int) totalBars * percent);
+        int leftOver = (totalBars - progressBars);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(ChatColor.translateAlternateColorCodes('&', completedColor));
+        for (int i = 0; i < progressBars; i++) {
+            sb.append(symbol);
+        }
+        sb.append(ChatColor.translateAlternateColorCodes('&', notCompletedColor));
+        for (int i = 0; i < leftOver; i++) {
+            sb.append(symbol);
+        }
+        return sb.toString();
+    }
+}
